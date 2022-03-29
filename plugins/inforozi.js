c7de4c8da5e2b9efedbeb37772b444e666926d1e
Klebitz12890 <97742308+Klebitz12890@users.noreply.github.com>
2022-03-29 15:54:13 +0700

    Update inforozi.js

--- plugins/inforozi.js
+++ plugins/inforozi.js
@@ -2,13 +2,13 @@ let handler  = async (m, { conn, usedPrefix }) => {
 conn.reply(m.chat, `
 ╭═══════════════════════
 ║╭──❉ 〔 INFO OWNER 〕 ❉────── 
-║│➸ ```NAMA``` : ROZI
-║│➸ ```UMUR``` : 15thn
-║│➸ ```ASAL``` : PONTIANAK
+║│➸ ```NAMA``` : FIKRI
+║│➸ ```UMUR``` : 12thn
+║│➸ ```ASAL``` : MEDAN
 ║│➸ ```OFFICIAL GRUP``` :
-https://chat.whatsapp.com/I8Q4oJVw8buHhIgMH5iVAv
-║│➸ ```ISTAGRAM``` : http://instagram.com/zalfapontianak
-║│➸ ```WHATSAPP``` : http://wa.me/6285828764046
+Belum Ada :v
+║│➸ ```ISTAGRAM``` : http://instagram.com/12345_zeez
+║│➸ ```WHATSAPP``` : http://wa.me/6281211006838
 ╰────────❉
 `.trim(), m)
 }
